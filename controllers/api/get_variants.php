5c067dadf2238c2df0db3b53b59ce3d4aa5c825a
Direwen <direwensik@gmail.com>
2024-11-23 09:17:24 +0630

    Fixed get_variatns api to return only variants which are not in the inventory yet

--- controllers/api/get_variants.php
+++ controllers/api/get_variants.php
@@ -9,13 +9,27 @@ $result = ErrorHandler::handle(fn () => $variant_model->getAll(
     select: [
         ["column" => $variant_model->getColumnId()],
         ["column" => $variant_model->getColumnName()],
-        ["column" => $product_model->getColumnName(), "alias" => "product_name", "table" => $product_model->getTableName()]
+        ["column" => $product_model->getColumnName(), "alias" => "product_name", "table" => $product_model->getTableName()],
+        ["column" => $inventory_model->getColumnCode(), "alias" => "inventory_code", "table" => $inventory_model->getTableName()]
     ],
     joins: [
         [
             'type' => 'INNER JOIN',
             'table' => $product_model->getTableName(),
             'on' => "variants.product_id = products.id",
+        ],
+        [
+            'type' => 'LEFT JOIN',
+            'table' => $inventory_model->getTableName(),
+            'on' => "inventories.variant_id = variants.id",
+        ],
+
+    ],
+    conditions: [
+        [
+            'attribute' => $inventory_model->getTableName() . '.' . $inventory_model->getColumnId(),
+            'operator' => "IS",
+            'value' => null
         ]
     ]
 ));
